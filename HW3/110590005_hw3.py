a1b78e5d1137877b90b7c42a5c5cd23d4b790541
vincent <hankggg2003xd@gmail.com>
2024-04-30 14:54:54 +0800

    hw3⭐

--- HW3/110590005_hw3.py
+++ HW3/110590005_hw3.py
@@ -28,7 +28,18 @@ def color_image(image_input):
     return result
 
 
-def eight_distance_transform(image_input):
+def get_eight_neighbors(image_input, i, j):
+    return np.array([image_input[i - 1, j - 1: j + 2], image_input[i, j - 1: j + 2], image_input[i + 1, j - 1: j + 2]])
+
+
+def get_maximal_distance_in_neighbors(image_input, i, j):
+    neighbors = get_eight_neighbors(image_input, i, j)
+    flatten_neighbors = neighbors.flatten()
+    flatten_neighbors = np.delete(flatten_neighbors, 4)
+    return np.max(flatten_neighbors)
+
+
+def four_distance_transform(image_input):
     b_image = image_input.copy()
     b_image = np.pad(b_image, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
     height, width = image_input.shape
@@ -36,80 +47,104 @@ def eight_distance_transform(image_input):
     pre_depth = depth
     while True: 
         max_depth = 0
-        for i in range(1, height + 1):
-            for j in range(1, width + 1):
+        for j in range(1, width + 1):
+            for i in range(1, height + 1):
                 if b_image[i, j] == depth:
-                    eight_neighbors = [
-                        b_image[i - 1, j - 1], b_image[i - 1, j], b_image[i - 1, j + 1],
-                        b_image[i, j - 1], b_image[i, j + 1],
-                        b_image[i + 1, j - 1], b_image[i + 1, j], b_image[i + 1, j + 1]
+                    four_neighbors = [
+                        b_image[i - 1, j],
+                        b_image[i, j - 1],
+                        b_image[i, j + 1],
+                        b_image[i + 1, j],
                     ]
-                    min_distance = min(eight_neighbors)
+                    min_distance = min(four_neighbors)
                     max_depth = min_distance
                     b_image[i, j] = min_distance + 1
         if max_depth == pre_depth:
             break
         pre_depth = max_depth
         depth += 1
-    b_image = b_image[1:-1, 1:-1]
-    return b_image, depth
+    return b_image[1:-1, 1:-1]
+
+
+def match_pattern(neighbors):
+    neighbors_copy = neighbors.copy()
+    neighbors_copy[1, 1] = 0 
+    if np.sum(neighbors_copy[1, :]) == 0 and np.sum(neighbors_copy[2, :]) != 0 and np.sum(neighbors_copy[0, :]) != 0:
+        return True
+    elif np.sum(neighbors_copy[:, 1]) == 0 and np.sum(neighbors_copy[:, 2]) != 0 and np.sum(neighbors_copy[:, 0]) != 0:
+        return True
+    elif neighbors_copy[1, 0] != 0 and neighbors_copy[0, 1] != 0 and neighbors_copy[2, 2] != 0 and neighbors_copy[1, 2] == 0 and neighbors_copy[2, 1] == 0:
+        return True
+    elif neighbors_copy[1, 2] != 0 and neighbors_copy[0, 1] != 0 and neighbors_copy[2, 0] != 0 and neighbors_copy[1, 0] == 0 and neighbors_copy[2, 1] == 0:
+        return True
+    elif neighbors_copy[1, 2] != 0 and neighbors_copy[2, 1] != 0 and neighbors_copy[0, 0] != 0 and neighbors_copy[0, 1] == 0 and neighbors_copy[1, 0] == 0:
+        return True
+    elif neighbors_copy[1, 0] != 0 and neighbors_copy[2, 1] != 0 and neighbors_copy[0, 2] != 0 and neighbors_copy[0, 1] == 0 and neighbors_copy[1, 2] == 0:
+        return True
+    elif ((neighbors_copy[0, 0] != 0 and neighbors_copy[2, 2] != 0) or (neighbors_copy[2, 0] != 0 and neighbors_copy[2, 0] != 0)) and neighbors_copy[1, 2] == 0 and neighbors_copy[2, 1] == 0 and neighbors_copy[1, 0] == 0 and neighbors_copy[0, 1] == 0: 
+        return True
+    return False
 
 
 def remain_local_maxima(image_input):
-    b_image = image_input.copy()
-    b_image = np.pad(b_image, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
+    result_image = image_input.copy()
+    distance_image = image_input.copy()
+    result_image = np.pad(result_image, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
+    distance_image = np.pad(distance_image, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
     height, width = image_input.shape
-    result = np.zeros((height + 2, width + 2))
+    depth = np.max(image_input)
+    for d in range(1, depth+1):
+        for i in range(1, height + 1):
+            for j in range(1, width + 1):
+                if distance_image[i, j] == d:
+                    eight_neighbors = get_eight_neighbors(result_image, i, j)
+                    max_distance = get_maximal_distance_in_neighbors(distance_image, i, j)
+                    if max_distance > distance_image[i, j] and not match_pattern(eight_neighbors):
+                        result_image[i, j] = 0
+    return result_image[1:-1, 1:-1]
+
+def remove_redundant_pixels(image_input):
+    result_image = image_input.copy()
+    result_image = np.pad(result_image, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
+    height, width = image_input.shape
     for i in range(1, height + 1):
         for j in range(1, width + 1):
-            if b_image[i, j] > 0:
-                eight_neighbors = [
-                    # b_image[i-1, j-1], 
-                    b_image[i-1, j], 
-                    # b_image[i-1, j+1],
-                    
-                    b_image[i, j-1], 
-                    b_image[i, j + 1],
-                    
-                    # b_image[i+1, j-1], 
-                    b_image[i+1, j], 
-                    # b_image[i+1, j+1]
-                ]
-                max_distance = max(eight_neighbors)
-                if max_distance == b_image[i, j]:
-                    result[i, j] = 1
+            if result_image[i, j] != 0 and result_image[i, j + 1] != 0:
+                eight_neighbors = get_eight_neighbors(result_image, i, j)
+                for u in range(3):
+                    if u == 1:
+                        continue
+                    for v in range(3):
+                        if eight_neighbors[u, v] != 0:
+                            target_neighbors = get_eight_neighbors(result_image, i + u - 1, j + v - 1)
+                            if not match_pattern(target_neighbors):
+                                result_image[i + u - 1, j + v - 1] = 0
+            if result_image[i, j] != 0 and result_image[i + 1, j] != 0:
+                eight_neighbors = get_eight_neighbors(result_image, i, j)
+                for u in range(3):
+                    for v in range(3):
+                        if v == 1:
+                            continue
+                        if eight_neighbors[u, v] != 0:
+                            target_neighbors = get_eight_neighbors(result_image, i + u - 1, j + v - 1)
+                            if not match_pattern(target_neighbors):
+                                result_image[i + u - 1, j + v - 1] = 0
+            if result_image[i - 1, j] != 0 and result_image[i, j - 1] != 0:
+                target_neighbors = get_eight_neighbors(result_image, i - 1, j - 1)
+                if not match_pattern(target_neighbors):
+                    result_image[i - 1, j - 1] = 0
+                target_neighbors = get_eight_neighbors(result_image, i, j)
+                if not match_pattern(target_neighbors):
+                    result_image[i, j] = 0
+            if result_image[i, j] != 0 and result_image[i - 1, j - 1] != 0:
+                target_neighbors = get_eight_neighbors(result_image, i, j - 1)
+                if not match_pattern(target_neighbors):
+                    result_image[i, j - 1] = 0
+                target_neighbors = get_eight_neighbors(result_image, i - 1, j)
+                if not match_pattern(target_neighbors):
+                    result_image[i - 1, j] = 0
+    return result_image[1:-1, 1:-1]
 
-    result = result[1:-1, 1:-1]
-    return result
-
-
-# def can_be_removed(b_image, tlx, tly, brx, bry):
-#     unit = b_image[tlx:brx + 1, tly:bry + 1]
-#     unit[1, 1] = 0
-#     orphan = 0
-#     for y in range(3):
-#         for x in range(3):
-#             if unit[y, x] > 0:
-#                 eight_neighbors = [n for n in 
-#                         [
-#                         unit[y - 1, x - 1] if y - 1 >= 0 and x - 1 >= 0 else 0, 
-#                         unit[y - 1, x] if y - 1 >= 0 else 0,
-#                         unit[y - 1, x + 1] if y - 1 >= 0 and x + 1 < 3 else 0,
-
-#                         unit[y, x - 1] if x - 1 >= 0 else 0,
-#                         unit[y, x + 1] if x + 1 < 3 else 0,
-                        
-#                         unit[y + 1, x - 1] if y + 1 < 3 and x - 1 >= 0 else 0,
-#                         unit[y + 1, x] if y + 1 < 3 else 0,
-#                         unit[y + 1, x + 1] if y + 1 < 3 and x + 1 < 3 else 0
-#                     ]
-#                     if n != 0
-#                 ]
-#                 if len(eight_neighbors) == 0:
-#                     orphan += 1
-#     if orphan > 1:
-#         return False
-#     return True
 
 def erosion(b_image, kernel):
     height, width = b_image.shape
@@ -119,8 +154,7 @@ def erosion(b_image, kernel):
             if b_image[i, j] == 1:
                 if np.array_equal(b_image[i : i + 3, j : j + 3], kernel):
                     result[i, j] = 1
-    result = result.astype(np.uint8)
-    return result
+    return result.astype(np.uint8)
 
 
 def dilation(b_image, kernel):
@@ -131,8 +165,7 @@ def dilation(b_image, kernel):
             if b_image[i, j] == 1:
                 result[i + 1 : i + 4, j + 1 : j + 4] = kernel
     result = result.astype(np.uint8)
-    result = result[1:-1, 1:-1]
-    return result
+    return result[1:-1, 1:-1]
 
 
 def closing(image_input, max_iter = 1):
@@ -163,43 +196,17 @@ def opening(image_input, max_iter = 1):
     return result
 
 
-def remove_depth_pixels(image_input, depth):
-    result = image_input.copy()
-    result = np.where(result < depth, 0, 1).astype(np.uint8)
-    return result
-
-
 if __name__ == '__main__':
-    for i in range(4, 6):
+    for i in range(1, 5):
         image = cv2.imread(f'images/img{i}.jpg')
-        h, w = image.shape[:2]
         binary_image = rgb_to_binary(image)
         if i == 1:
             binary_image = closing(binary_image, 2)
             binary_image = opening(binary_image, 2)
-
-        # q1_1
-        result_1, depth = eight_distance_transform(binary_image)
-        result_color = distance_color_image(result_1, depth)
+        result_1 = four_distance_transform(binary_image)
+        result_color = distance_color_image(result_1, np.max(result_1))
         cv2.imwrite(f'results/img{i}_q1_1.jpg', result_color)
-        
         result_2 = remain_local_maxima(result_1)
-        # kernel = np.array([
-        #     [1, 1, 1],
-        #     [1, 1, 1],
-        #     [1, 1, 1]
-        # ])
-        # result_2 = dilation(result_2, kernel)
-        # result_2 = dilation(result_2, kernel)
-        # result_2 = erosion(result_2, kernel)
-        result_2 = np.where(result_2 == 1, 255, 0).astype(np.uint8)
-        cv2.imwrite(f'dev/img{i}_q1_2.jpg', result_2)
-        # with open(f'txts/img{i}_q1_2.txt', 'w') as f:
-        #     for row in result_m:
-        #         f.write(' '.join([str(x) for x in row]) + '\n')
-        
-        
-        # cv2.imwrite(f'dev/img{i}_q1_2.jpg', result_color)
-        # break
-        # resultf = medial_axis(result)
-        # cv2.imwrite(f'results/img{i}_q1_2.jpg', resultf)
+        result_2 = remove_redundant_pixels(result_2)
+        result_2_color = np.where(result_2 > 0, 255, 0).astype(np.uint8)
+        cv2.imwrite(f'results/img{i}_q1_2.jpg', result_2_color)
